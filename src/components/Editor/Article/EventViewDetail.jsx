f05d5c52dd46775a450d90ae6941f5322a5a0fcd
Tructhanh2012 <tructhanh15994@gmail.com>
2023-07-16 13:27:27 +0700

    preview article

---
+++ src/components/Editor/Article/EventViewDetail.jsx
@@ -0,0 +1,58 @@
+import { Descriptions, Divider, Modal } from "antd";
+import { useState } from "react";
+import ReactHTMLParser from "html-react-parser";
+
+const EventViewDetail = (props) => {
+  const {
+    dataViewDetail,
+    setDataViewDetail,
+    openViewDetail,
+    setOpenViewDetail,
+  } = props;
+
+  const onOpen = () => {
+    setOpenViewDetail(false);
+  };
+  const onClose = () => {
+    setOpenViewDetail(false);
+  };
+  return (
+    <>
+      <Modal
+        title="Xem bài viết"
+        width={"70vw"}
+        open={openViewDetail}
+        onCancel={onClose}
+        footer={null}
+      >
+        <Divider />
+        <Descriptions title="Thông tin Editor:">
+          <Descriptions.Item label="Id">
+            {dataViewDetail?.editorInfor.id}
+          </Descriptions.Item>
+          <Descriptions.Item label="Email">
+            {dataViewDetail?.editorInfor.email}
+          </Descriptions.Item>
+          <Descriptions.Item label="Tên">
+            {dataViewDetail?.editorInfor.name}
+          </Descriptions.Item>
+        </Descriptions>
+
+        <Descriptions title="Thông tin bài viết:" bordered column={2}>
+          <Descriptions.Item label="ID">
+            {dataViewDetail?.articleId}
+          </Descriptions.Item>
+          <Descriptions.Item label="Tiêu đề">
+            {dataViewDetail?.title}
+          </Descriptions.Item>
+          <Descriptions.Item label="Tiêu đề">
+            {ReactHTMLParser(dataViewDetail?.content)}
+            {/* {dataViewDetail?.content} */}
+          </Descriptions.Item>
+        </Descriptions>
+      </Modal>
+    </>
+  );
+};
+
+export default EventViewDetail;
